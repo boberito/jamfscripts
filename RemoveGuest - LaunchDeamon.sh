a20631731d674a6197d9ca86a85d306a3d7e13d2
Bob Gendler <boberito@mac.com>
2018-07-18 23:30:37 -0400

    Update RemoveGuest - LaunchDeamon.sh

--- RemoveGuest - LaunchDeamon.sh
+++ RemoveGuest - LaunchDeamon.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-mkdir -p /usr/local/saes
-#touch /usr/local/saes/guestResults.txt
+#Sets up a script and creates a launchdeamon that watches com.apple.airport.preferences.plist
+#This changes whenever you join a network. If the Guest network is joined, then remove it and bop them off of it
+
 
 ##################
 ##CREATE SCRIPT###
 ##################
-cat << EOF > /usr/local/saes/removeGuest.sh
+cat << EOF > /usr/local/removeGuest.sh
 #!/bin/bash
 
 #set interface name and network you're hunting for
 interfaceName="Wi-Fi"
-networkName="attwifi"
+networkName="GUEST"
 
 Adapter=\$(networksetup -listallhardwareports | grep -A 1 "\$interfaceName" | grep Device | awk '{print \$2}')
 
@@ -23,10 +24,10 @@ if networksetup -listpreferredwirelessnetworks \$Adapter | grep "\$networkName";
 			networksetup -setairportpower \$Adapter off
 			networksetup -removepreferredwirelessnetwork \$Adapter "\$networkName"
 			networksetup -setairportpower \$Adapter on
-			#echo "Guest Connected and Found and Removed" >> /usr/local/saes/guestResults.txt
+			
 	else
 			networksetup -removepreferredwirelessnetwork \$Adapter "\$networkName"
-			#echo "Guest Found and Removed" >> /usr/local/saes/guestResults.txt
+			
 	fi
 fi
 
@@ -36,18 +37,18 @@ EOF
 ##CREATE LAUNCHDAEMON###
 ########################
 
-cat << EOF > /Library/LaunchDaemons/com.saes.removeguest.plist
+cat << EOF > /Library/LaunchDaemons/com.YOURCOMPANY.removeguest.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
 	<key>Label</key>
-	<string>com.saes.removeguest2</string>
+	<string>com.YOURCOMPANY.removeguest2</string>
 	<key>ProgramArguments</key>
 	<array>
 		<string>sh</string>
 		<string>-c</string>
-		<string>/usr/local/saes/removeGuest.sh</string>
+		<string>/usr/local/removeGuest.sh</string>
 	</array>
 	<key>WatchPaths</key>
 	<array>
@@ -58,10 +59,10 @@ cat << EOF > /Library/LaunchDaemons/com.saes.removeguest.plist
 
 EOF
 
-chown rgendler:wheel /usr/local/saes/removeGuest.sh
-chmod 755 /usr/local/saes/removeGuest.sh
+chown rgendler:wheel /usr/local/removeGuest.sh
+chmod 755 /usr/local/removeGuest.sh
 
-chown root:wheel /Library/LaunchDaemons/com.saes.removeguest.plist
-chmod 644 /Library/LaunchDaemons/com.saes.removeguest.plist
+chown root:wheel /Library/LaunchDaemons/com.YOURCOMPANY.removeguest.plist
+chmod 644 /Library/LaunchDaemons/com.YOURCOMPANY.removeguest.plist
 
-launchctl load -w /Library/LaunchDaemons/com.saes.removeguest.plist
+launchctl load -w /Library/LaunchDaemons/com.YOURCOMPANY.removeguest.plist
